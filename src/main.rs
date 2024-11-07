5fef28a0f7e188aa32ca9709d1d58d04d8111855
Scott Perkins <whatisbyandby@gmail.com>
2024-11-06 17:43:11 -0700

    Switch the USB to USB Logger

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ use embassy_rp::flash::{Blocking, Flash};
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIO0, USB};
 use embassy_rp::pio::{InterruptHandler as PioInterruptHandler, Pio};
-use embassy_rp::usb::{Driver, Instance, InterruptHandler as UsbInterruptHandler};
-use embassy_usb::class::cdc_acm::{CdcAcmClass, Receiver, State};
-use embassy_usb::driver::EndpointError;
-use embassy_usb::UsbDevice;
-use embassy_rp::watchdog::Watchdog;
 
+use embassy_rp::usb::{Driver, InterruptHandler as UsbInterruptHandler};
+use embassy_rp::watchdog::Watchdog;
+use embassy_rp::rom_data::reset_to_usb_boot;
 use embassy_net::{Config as NetConfig, Stack, StackResources};
 use embassy_net_driver_channel::Device as D;
+use embassy_usb_logger::ReceiverHandler;
+use core::str;
 
 use rand::RngCore;
 
@@ -49,14 +49,14 @@ use embassy_sync::channel::{Channel as SyncChannel, Sender};
 static COMMAND_CHANNEL: SyncChannel<ThreadModeRawMutex, Result<PicoCommand, &'static str>, 5> =
     SyncChannel::new();
 
-#[embassy_executor::task]
-async fn command_task(mut receiver: Receiver<'static, Driver<'static, USB>>) {
-    receiver.wait_connection().await;
-    loop {
-        let command = get_command(&mut receiver).await;
-        COMMAND_CHANNEL.send(command).await;
-    }
-}
+// #[embassy_executor::task]
+// async fn command_task(mut receiver: Receiver<'static, Driver<'static, USB>>) {
+//     receiver.wait_connection().await;
+//     loop {
+//         let command = get_command(&mut receiver).await;
+//         COMMAND_CHANNEL.send(command).await;
+//     }
+// }
 
 #[embassy_executor::task]
 async fn cyw43_task(
@@ -139,65 +139,18 @@ async fn main(spawner: Spawner) {
 
     // Create the driver, from the HAL.
     let driver = Driver::new(p.USB, UsbIrqs);
+    spawner.spawn(logger_task(driver)).unwrap();
 
     // Create the ADC and the temperature sensor channel.
     let adc = Adc::new(p.ADC, AdcIrqs, Config::default());
     let ts = AdcChannel::new_temp_sensor(p.ADC_TEMP_SENSOR);
+   
 
-    // Create embassy-usb Config
-    let config = {
-        let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
-        config.manufacturer = Some("Paradigm");
-        config.product = Some("Mx Meetup Demo Device");
-        config.serial_number = Some(&static_id.as_str());
-        config.max_power = 100;
-        config.max_packet_size_0 = 64;
+    log::info!("Starting up!");
 
-        // Required for windows compatibility.
-        // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
-        config.device_class = 0xEF;
-        config.device_sub_class = 0x02;
-        config.device_protocol = 0x01;
-        config.composite_with_iads = true;
-        config
-    };
-
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
-    let mut builder = {
-        static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
-        static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
-        static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
-
-        let builder = embassy_usb::Builder::new(
-            driver,
-            config,
-            CONFIG_DESCRIPTOR.init([0; 256]),
-            BOS_DESCRIPTOR.init([0; 256]),
-            &mut [], // no msos descriptors
-            CONTROL_BUF.init([0; 64]),
-        );
-        builder
-    };
-
-    // Create classes on the builder.
-    let class = {
-        static STATE: StaticCell<State> = StaticCell::new();
-        let state = STATE.init(State::new());
-        CdcAcmClass::new(&mut builder, state, 64)
-    };
-
-    // Build the builder.
-    let usb = builder.build();
-
-    let (sender, receiver) = class.split();
 
     // Run the network stack.
     unwrap!(spawner.spawn(net_task(static_stack)));
-    // Run the USB device.
-    unwrap!(spawner.spawn(usb_task(usb)));
-    // Run the command task.
-    unwrap!(spawner.spawn(command_task(receiver)));
 
     let command_receiver: embassy_sync::channel::Receiver<
         '_,
@@ -210,7 +163,6 @@ async fn main(spawner: Spawner) {
         .with_id(static_id)
         .with_stack(static_stack)
         .with_command_receiver(command_receiver)
-        .with_usb_sender(sender)
         .with_control(control)
         .with_adc(adc, ts)
         .with_watchdog(watchdog)
@@ -219,48 +171,61 @@ async fn main(spawner: Spawner) {
 
     demo_device.init().await;
 
-    info!("Device Initalized!");
+    log::info!("Device Initalized!");
 
     demo_device.run().await;
 }
 
-type MyUsbDriver = Driver<'static, USB>;
-type MyUsbDevice = UsbDevice<'static, MyUsbDriver>;
+struct Handler;
+
+impl ReceiverHandler for Handler {
+    async fn handle_data(&self, data: &[u8]) {
+        if let Ok(data) = str::from_utf8(data) {
+            let data = data.trim();
+
+            // If you are using elf2uf2-term with the '-t' flag, then when closing the serial monitor,
+            // this will automatically put the pico into boot mode
+            if data == "q" || data == "elf2uf2-term" {
+                log::info!("Resetting to USB boot mode");
+                reset_to_usb_boot(0, 0); // Restart the chip
+            } else if data.eq_ignore_ascii_case("hello") {
+                log::info!("World!");
+            } else {
+                log::info!("Recieved: {:?}", data);
+            }
+        }
+    }
+
+    fn new() -> Self {
+        Self
+    }
+}
 
 #[embassy_executor::task]
-async fn usb_task(mut usb: MyUsbDevice) -> ! {
-    usb.run().await
+async fn logger_task(driver: Driver<'static, USB>) {
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver, Handler);
 }
 
-struct Disconnected {}
 
-impl From<EndpointError> for Disconnected {
-    fn from(val: EndpointError) -> Self {
-        match val {
-            EndpointError::BufferOverflow => panic!("Buffer overflow"),
-            EndpointError::Disabled => Disconnected {},
-        }
-    }
-}
 
-async fn get_command<'d, T: Instance + 'd>(
-    class: &mut Receiver<'d, Driver<'d, T>>,
-) -> Result<PicoCommand, &'static str> {
-    let mut full_data: heapless::String<256> = heapless::String::new();
-    let mut done = false;
-    while !done {
-        let mut buf = [0; 64];
-        let num_chars_read = class.read_packet(&mut buf).await.unwrap();
-        let packet_data = &buf[..num_chars_read];
-        full_data
-            .push_str(from_utf8(packet_data).unwrap())
-            .map_err(|_| "Command contains non-UTF characters")?;
-        info!("{}", full_data.as_str());
-        if num_chars_read < 64 {
-            done = true;
-        }
-    }
+// async fn get_command<'d, T: Instance + 'd>(
+//     class: &mut Receiver<'d, Driver<'d, T>>,
+// ) -> Result<PicoCommand, &'static str> {
+//     let mut full_data: heapless::String<256> = heapless::String::new();
+//     let mut done = false;
+//     while !done {
+//         let mut buf = [0; 64];
+//         let num_chars_read = class.read_packet(&mut buf).await.unwrap();
+//         let packet_data = &buf[..num_chars_read];
+//         full_data
+//             .push_str(from_utf8(packet_data).unwrap())
+//             .map_err(|_| "Command contains non-UTF characters")?;
+//         info!("{}", full_data.as_str());
+//         if num_chars_read < 64 {
+//             done = true;
+//         }
+//     }
 
-    let command = parse_command(full_data)?;
-    Ok(command)
-}
+//     let command = parse_command(full_data)?;
+//     Ok(command)
+// }
